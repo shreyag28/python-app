db55e0ab4ee70474ea9604818142ce5f2f16fa5f
shreyag28 <76720325+shreyag28@users.noreply.github.com>
2023-09-07 11:39:25 +0530

    Update models.py

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
