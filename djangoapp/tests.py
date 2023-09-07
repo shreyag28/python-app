6dde4e9f07a86fd8680c3b7d64c2ef83b9f94476
shreyag28 <76720325+shreyag28@users.noreply.github.com>
2023-09-07 11:38:47 +0530

    Update tests.py

--- djangoapp/tests.py
+++ djangoapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.db import IntegrityError  # Import IntegrityError
+from djangoapp.models import User
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        # Create a User instance for testing
+        self.user = User.objects.create(name='John Doe', email='john@example.com')
+
+    def test_user_creation(self):
+        # Test that the user was created correctly
+        self.assertEqual(self.user.name, 'John Doe')
+        self.assertEqual(self.user.email, 'john@example.com')
+
+    def test_user_str_method(self):
+        # Test the __str__ method of the User model
+        self.assertEqual(str(self.user), 'John Doe')
+
+
+    def test_user_name_max_length(self):
+        # Test the max length of the name field
+        max_length = self.user._meta.get_field('name').max_length
+        self.assertEqual(max_length, 50)
+
+    def test_user_email_max_length(self):
+        # Test the max length of the email field
+        max_length = self.user._meta.get_field('email').max_length
+        self.assertEqual(max_length, 50)
